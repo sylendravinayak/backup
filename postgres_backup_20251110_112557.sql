ccf11e03199492c5439a71ad7399c1c8efcb0509
Sylendra vinayak R <124984380+sylendravinayak@users.noreply.github.com>
2025-11-10 16:55:59 +0530

    Backup: postgres_backup_20251110_112557.sql

---
+++ postgres_backup_20251110_112557.sql
@@ -0,0 +1,50 @@
+--
+-- PostgreSQL database dump
+--
+
+\restrict ahui4TvYSvPAdRDHayoSqicO0pbuxWTy9Ld9p7XH8FiGE0TnaC8Znu4bXAsIVKV
+
+-- Dumped from database version 17.6
+-- Dumped by pg_dump version 17.6
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP TABLE IF EXISTS public.bacup;
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: bacup; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.bacup (
+);
+
+
+ALTER TABLE public.bacup OWNER TO postgres;
+
+--
+-- Data for Name: bacup; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.bacup  FROM stdin;
+\.
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+\unrestrict ahui4TvYSvPAdRDHayoSqicO0pbuxWTy9Ld9p7XH8FiGE0TnaC8Znu4bXAsIVKV
+
